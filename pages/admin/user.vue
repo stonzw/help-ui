81080c254e63f20fab53aa4eecacc6b8da84732b
SotaOnozawa <adopqr0469@gmail.com>
2020-04-26 17:55:46 +0900

    refactoring desing

--- pages/admin/user.vue
+++ pages/admin/user.vue
@@ -4,32 +4,34 @@
       <v-row>
         <v-col v-for="depId in Object.keys(employeeInfos)" :key="`employee-infos-${depId}`" cols="12">
           <h2>{{ departmentId2Name[depId] }}</h2>
-          <v-card v-for="userInfo in employeeInfos[depId]" :key="`employee-info-${userInfo.id}`">
-            <v-card-title>{{ userInfo.name }}</v-card-title>
-            <v-card-text v-if="editableId != userInfo.id">
-              部署: {{ departmentId2Name[userInfo.department_id] }}
-              <v-btn @click="clickEditButton(userInfo.id, userInfo.department_id)" text block color="green">
-                部署を編集する
-                <v-icon>
-                  mdi-pencil
-                </v-icon>
-              </v-btn>
-            </v-card-text>
-            <v-card-text v-else>
-              <v-select
-                v-model="selectedDepartment"
-                :items="departments"
-                item-text="name"
-                item-value="id"
-              />
-              <v-btn @click="clickEditSaveButton(userInfo, userInfo.id, userInfo.department_id)" color="primary" block>
-                保存
-              </v-btn>
-              <v-btn @click="editableId=-1" color="red" dark block>
-                キャンセル
-              </v-btn>
-            </v-card-text>
-          </v-card>
+          <v-col v-for="userInfo in employeeInfos[depId]" :key="`employee-info-${userInfo.id}`">
+            <v-card>
+              <v-card-title>{{ userInfo.name }}</v-card-title>
+              <v-card-text v-if="editableId != userInfo.id">
+                部署: {{ departmentId2Name[userInfo.department_id] }}
+                <v-btn @click="clickEditButton(userInfo.id, userInfo.department_id)" text block color="green">
+                  部署を編集する
+                  <v-icon>
+                    mdi-pencil
+                  </v-icon>
+                </v-btn>
+              </v-card-text>
+              <v-card-text v-else>
+                <v-select
+                  v-model="selectedDepartment"
+                  :items="departments"
+                  item-text="name"
+                  item-value="id"
+                />
+                <v-btn @click="clickEditSaveButton(userInfo, userInfo.id, userInfo.department_id)" color="primary" block>
+                  保存
+                </v-btn>
+                <v-btn @click="editableId=-1" color="red" dark block>
+                  キャンセル
+                </v-btn>
+              </v-card-text>
+            </v-card>
+          </v-col>
         </v-col>
       </v-row>
     </v-container>
