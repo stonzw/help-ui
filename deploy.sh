b32372f0333f8d67f03e39c4bd6fa5596c0d1bfc
SotaOnozawa <adopqr0469@gmail.com>
2020-09-23 13:58:20 +0900

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@
 [ -s "$HOME/.nvm/nvm.sh" ] && source "$HOME/.nvm/nvm.sh" && nvm use
 # まだインストールされていない場合は npm をインストールする
 [ ! -d "node_modules" ] && npm install
-process.env.API_URL=https://help-api-cloud-run-service-iv44qe7s4q-an.a.run.app/
 
 npm run generate
-gulp deploy
+aws s3 sync dist/ s3://www.help-knowledge.com --profile=help-ui
